5d3b45214f2915b3fa5e8453891af44ac6281126
tomo1123 <tomocruise01@gmail.com>
2023-10-12 22:37:55 +0900

    ユーザー管理ディバイスインストール

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,9 +11,9 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :encrypted_password, presence: true
   validates :password_confirmation, presence: true
-  validates :family_name, presence: true
+  validates :last_name, presence: true
   validates :first_name, presence: true
-  validates :family_name_kana, presence: true
+  validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
   
