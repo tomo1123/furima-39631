76850f3ae656ddc5ad5ffc1b8b370654584a6264
tomo1123 <tomocruise01@gmail.com>
2023-10-21 20:20:34 +0900

    テスト機能追記２

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -95,9 +95,9 @@ RSpec.describe Item, type: :model do
       it 'ユーザーが紐付いていないと商品は出品できない' do
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("User can't be blank") 
+        expect(@item.errors.full_messages).to include("User must exist")
       end
-      
+
     end
   end
 end
