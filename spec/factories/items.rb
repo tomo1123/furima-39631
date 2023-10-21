678a77c97e1caff24b0087c8b0961fa93507a65f
tomo1123 <tomocruise01@gmail.com>
2023-10-21 11:19:42 +0900

    テストコード修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,19 +1,17 @@
 FactoryBot.define do
   factory :item do
-    name { '商品名' }
-    description { '商品の説明' }
+    title { '商品名' }
+    items_text { '商品の説明' }
     category_id { 2 }
     condition_id { 2 }
-    shipping_fee_id { 2 }
+    delivery_fee_id { 2 }
     prefecture_id { 2 }
-    days_to_ship_id { 2 }
+    delivery_day_id { 2 }
     price { 300 }
 
-    association :user
-
     after(:build) do |item|
-      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      item.image.attach(io: File.open('public/test_image.png'), filename: 'test_image.png')
     end
-    
+  
   end
 end
