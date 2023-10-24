a5036f4b665065720bc2154fb836b57fb117e624
tomo1123 <tomocruise01@gmail.com>
2023-10-24 23:12:39 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -31,7 +31,15 @@ class ItemsController < ApplicationController
     end
   end
 
-  
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
 
 
   private
