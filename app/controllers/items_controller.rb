c2ce6592c7fca3763783bb14d3f39bf3d352c71b
tomo1123 <tomocruise01@gmail.com>
2023-10-25 18:41:12 +0900

    指摘事項修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,9 @@
 class ItemsController < ApplicationController
 
-  before_action :authenticate_user!, only: [:new, :create]
+
+  before_action :set_item, only: [:edit, :update, :show]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  
 
   def index
     @items = Item.order("created_at DESC")
@@ -20,12 +23,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+    
     @items = Item.all
   end
 
   def edit
-    @item = Item.find(params[:id])
+    @item_user = Item.find_by(id: params[:id])
     if @item_user.id.nil?
       redirect_back_or_to root_path, alert: 'Item not found.'
       return
@@ -36,7 +39,6 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -47,6 +49,11 @@ class ItemsController < ApplicationController
 
 
   private
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
     params.require(:item).permit(:title, :items_text, :category_id, :condition_id, :delivery_fee_id, :prefecture_id, :delivery_day_id, :price, :image).merge(user_id: current_user.id)
   end
